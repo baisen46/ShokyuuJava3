44f9db6ffaf5bef22427d4fda4b3506810a5f14f
chiba <baisen46@gmail.com>
2024-05-13 11:54:10 +0900

    初級Java2.5のコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,22 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 3;
+		 
+		System.out.println (x*y);
+			
+	
+		
+		MyFrame frame2 = new RectFrame();
+		frame2.setLocation(250,300);
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
+	
+		
+
+	}
+
+}
