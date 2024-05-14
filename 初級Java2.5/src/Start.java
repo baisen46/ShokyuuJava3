ec593ed1891594e7c395a981d53fb80529c20d6e
chiba <baisen46@gmail.com>
2024-05-14 10:45:32 +0900

    3.1までやった。

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -2,21 +2,9 @@
 public class Start {
 
 	public static void main(String[] args) {
-		
-		int x = 2;
-		int y = 3;
-		 
-		System.out.println (x*y);
-			
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectTest();
 	
-		
-		MyFrame frame2 = new RectFrame();
-		frame2.setLocation(250,300);
-		MyFrame frame3=new RectFrame();
-		frame3.setLocation(50,300);
-	
-		
-
 	}
 
 }
