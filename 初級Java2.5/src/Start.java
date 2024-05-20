5a31422fd8e029c4c4f83f317bbd948e4a37b4f0
chiba <baisen46@gmail.com>
2024-05-20 10:38:55 +0900

    練習問題3-1

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -1,9 +1,6 @@
-public class Start {
-
-	public static void main(String[] args) {
+public class Start{
+	public static void main (String[]args) {
 		System.out.println("プログラムを実行します");
-		MyFrame framel=new TextTest();
-	
+		MyFrame frame1=new RectAndOval();
 	}
-
 }
