eefcff055d711bfbe1568921aa1e822707aad791
chiba <baisen46@gmail.com>
2024-05-20 10:54:24 +0900

    4.1

---
+++ 初級Java2.5/src/RectTest.java
@@ -0,0 +1,20 @@
+
+public class RectTest extends MyFrame {
+	public void run() {
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		
+		setColor(150,80,150);
+		fillRect(90,40,50,100);
+		
+		setColor(25,240,255);
+		
+		fillRect(150,40,50,100);
+		
+		setColor(0,0,0);
+		fillRect(210,40,50,100);
+		
+		setColor(100,100,100);
+		fillRect(270,40,50,100);
+	}
+}
---
+++ 初級Java2.5/src/src/OvalTest.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.awt.Frame;
+
+public class OvalTest extends Frame {
+	public void run() {
+		fillOval(30,40,50,100);
+		
+	}
+
+	private void fillOval(int i, int j, int k, int l) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
