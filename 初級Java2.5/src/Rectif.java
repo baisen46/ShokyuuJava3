907ccd0bc3ba8fdf05ce52fe877892e70423876c
chiba <baisen46@gmail.com>
2024-05-21 11:54:50 +0900

    5.1図形をゆっくり表示する

--- 初級Java2.5/src/Rectif.java
+++ 初級Java2.5/src/Rectif.java
@@ -2,18 +2,15 @@
 public class Rectif extends MyFrame{
 	public void run() {
 		int x=30;
-		
-	
+		setColor(0,128,0);
 		int i;
-		for (i=0 ; i<9 ; i++) 
-		{
+		for (i=0 ; i<9 ; i++) {
 			if(i==50)
-				fillRect(x,60,10,100);
+				fillRect(x,60,10,1);
 	
 			
 		else {
-			setColor(x,x,x);
-			fillRect(x,60,10,100);
+			fillRect(x,50,50,100);
 		}
 			x+=20;
 		}
