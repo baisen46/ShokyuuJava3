f9aa313066656139b00de6f432dcc2f0312e33ec
chiba <baisen46@gmail.com>
2024-05-27 10:47:41 +0900

    5.3 図形を永久に動かす

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,13 +2,16 @@
 public class Animation extends MyFrame {
 	public void run() {
 		int x=30;
-		while(x<=170) {
+		while(true) {
 			// (1)画面を消す
 			clear();
 			//(2)四角形を表示する
 			setColor(0,128,0);
-			fillOval(x,80,100,100);
+			fillRect(x,80,10,100);
 			x+=5;
+			if(x>200) { // xの値が200より大きくなったら
+				break; // whilによる繰り返しをやめる
+			}
 			sleep(0.1);
 		}
 	}
