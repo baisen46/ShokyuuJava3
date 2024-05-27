92d362982fd9320a8b60c33d2446569bed9739d5
chiba <baisen46@gmail.com>
2024-05-27 11:44:41 +0900

    練習問題5-2 最初の練習問題5.3は5-1

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,27 +1,32 @@
 
 public class Animation extends MyFrame {
 	public void run() {
-		int y = 220;
+		int y = 50;
 		int x = 30;
-		while (y >= 100) {
+		while (y <= 250) {
 			// (1)画面を消す
 			clear();
 			// (2)四角形を表示する
 			setColor(0, 128, 0);
-			fillRect(30, y, 10, 100); // xとyを用いて表示
-			y -= 5; // xを減らす(yは変更しない) 
-			sleep(0.1);
-		}
-		while (x <= 200) {
-			// (1)画面を消す
-			clear();
-			//(2)四角形を表示する
-			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100); // xとyを用いて表示
-			x += 5; // xを増やす(yは変更しない)
+			x += 5;
+			y += 5; // xを減らす(yは変更しない) 
 			sleep(0.1);
-
 		}
+		while(x>=70) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x-=5;
+			sleep(0.1);
+		}
+		while (x<=230) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
+			y-=5;
+			sleep(0.1);
+		}		
 	}
-
 }
