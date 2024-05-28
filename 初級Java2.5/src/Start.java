d02e23875c028286ebc73764640ae40ab801a6f8
chiba <baisen46@gmail.com>
2024-05-28 09:59:11 +0900

    6.1 顔の図形をいくつか描いてみる

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -2,7 +2,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プログラムを実行します");
-		MyFrame framel=new Animation();
+		MyFrame framel=new DisplayFaceObject();
 	
 	}
 
