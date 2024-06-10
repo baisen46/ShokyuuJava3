1d028d4b311fb4ea81766aa5be3c5d667a0e7671
chiba <baisen46@gmail.com>
2024-06-10 12:13:13 +0900

    9.2キーボードに反応して動くようにする

--- 初級Java2.5/src/Car.java
+++ 初級Java2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements KeyListener {
 
 	public Car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
@@ -15,4 +17,34 @@ public class Car extends Vehicle {
 
 	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)	{
+	    if(e.getKeyCode()==KeyEvent.VK_LEFT){
+	    	vx=0;
+	    }
+	    if (e.getKeyCode()==KeyEvent.VK_RIGHT){
+	    	vx=0;
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	}
 }
+
